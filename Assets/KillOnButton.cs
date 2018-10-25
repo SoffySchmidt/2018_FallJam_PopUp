8bb00c9b0cc07c8af4aa3296b32ee880764de850
TRUDgame <geag1850@edu.kadk.dk>
2018-10-25 20:19:04 +0200

    gert more shenanigans

---
+++ Assets/KillOnButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillOnButton : MonoBehaviour {
+
+    Image img;
+    public Image One;
+    public Image Two;
+
+    float _t = 0f;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(transform.childCount == 0f)
+        {
+            Destroy(gameObject);
+        }
+
+        img = GetComponent<Image>();
+
+        _t += Time.deltaTime;
+
+        if (_t >= 0.5f)
+        {
+            if (img == One)
+            {
+                img = Two;
+                _t = 0f;
+            }
+            else
+            {
+                img = One;
+                _t = 0f;
+            }
+
+        }
+
+    }
+}
