555f24c1288dafc0f41a89e7e9ccdf255e05fed9
TRUDgame <geag1850@edu.kadk.dk>
2018-10-25 15:34:52 +0200

    big ass meta files and randomizer and gitignore file.  - gertrud

---
+++ Assets/Randomizer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Randomizer : MonoBehaviour
+{
+
+    public GameObject[] spawnPoints;
+    GameObject currentPoint;
+    int index;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Popup();
+        }
+    }
+
+
+    void Popup()
+    {
+
+
+        //spawnPoints = GameObject.FindGameObjectsWithTag("point");
+        index = Random.Range(0, spawnPoints.Length);
+        currentPoint = spawnPoints[index];
+        print(currentPoint.name);
+
+
+        //GameObject spawned = currentPoint;
+        //spawned.transform.position = new Vector3(Random.value, Random.value, 0);
+
+        Instantiate(currentPoint, new Vector3(Random.Range(-7.0f, 7.0f), Random.Range(-4.0f, 4.0f)), Quaternion.identity);
+
+    }
+
+
+
+}
